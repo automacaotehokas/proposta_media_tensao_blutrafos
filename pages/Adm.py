3ce63453758044776fdf432d4fbef9d9ca58ea54
Tehokas Soluções <eric.martins@tehokas.com.br>
2024-10-16 14:16:34 -0300

    adm

--- pages/Adm.py
+++ pages/Adm.py
@@ -3,6 +3,7 @@ import pandas as pd
 import psycopg2
 from io import BytesIO
 from sqlalchemy import create_engine
+import openpyxl
 
 # Função para conectar ao banco de dados
 def get_db_connection():
@@ -38,24 +39,33 @@ st.title("Atualizar Base de Dados: custos_media_tensao")
 if st.button("Apagar todos os dados da tabela"):
     apagar_dados()
 
+# Campo para inserir a senha do arquivo Excel
+senha = st.text_input("Digite a senha para desbloquear o arquivo Excel (se necessário)", type="password")
+
 # Upload do arquivo Excel
 uploaded_file = st.file_uploader("Escolha o arquivo Excel com a planilha 'atualizacao'", type="xlsx")
 
 if uploaded_file:
     try:
-        # Leitura do arquivo Excel
-        df = pd.read_excel(uploaded_file, sheet_name='atualizacao')
+        # Leitura do arquivo Excel com senha
+        if senha:
+            # Se a senha for fornecida, tenta abrir o arquivo protegido
+            with BytesIO(uploaded_file.read()) as f:
+                excel_file = pd.read_excel(f, sheet_name='atualizacao', engine='openpyxl', password=senha)
+        else:
+            # Caso contrário, abre normalmente
+            excel_file = pd.read_excel(uploaded_file, sheet_name='atualizacao', engine='openpyxl')
 
         # Exibindo os dados carregados para conferência
         st.write("Dados carregados:")
-        st.dataframe(df)
+        st.dataframe(excel_file)
 
         # Verificação de layout
         expected_columns = ['p_caixa', 'p_trafo', 'potencia', 'custo', 'valor', 'classe', 'valor_ip_baixo', 'valor_ip_alto']
-        if all(col in df.columns for col in expected_columns):
+        if all(col in excel_file.columns for col in expected_columns):
             # Inserção dos dados no banco de dados
             if st.button("Inserir dados no banco de dados"):
-                inserir_dados(df)
+                inserir_dados(excel_file)
         else:
             st.error("A planilha não possui o layout esperado. Verifique as colunas.")
     
